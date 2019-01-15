dbed37666cda3b4411886fae8b70a9c78240d438
huhuiyu <1069306849@qq.com>
2019-01-15 20:16:12 +0800

    MyDataService文件上传功能

--- angularjs.utils/src/MyDataService.js
+++ angularjs.utils/src/MyDataService.js
@@ -25,7 +25,34 @@
     var service = {
       dataServer: '',
       before: angular.noop,
-      after: angular.noop
+      after: angular.noop,
+      beforeFile: angular.noop,
+      afterFile: angular.noop
+    };
+
+    service.isJson = function (obj) {
+      return obj && typeof (obj) == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length;
+    };
+
+    //表单参数名称合并
+    service.jsonToFlat = function (jsonObj, basename, outObj) {
+      if (!outObj) {
+        outObj = {};
+      }
+      for (key in jsonObj) {
+        var name = basename && basename.length > 0 ? basename + '.' + key : key;
+        var value = jsonObj[key];
+        if (!value) {
+          continue;
+        }
+        if (service.isJson(value)) {
+          service.jsonToFlat(value, name, outObj);
+        } else {
+          outObj[name] = value;
+          $log.debug(name, value);
+        }
+      }
+      return outObj;
     };
 
     //设置后台服务器地址
@@ -43,11 +70,22 @@
       service.after = afterFn;
     };
 
+    //设置上传文件请求前要执行的function
+    service.setBeforeFile = function (beforeFn) {
+      service.beforeFile = beforeFn;
+    };
+
+    //设置上传文件数据回来后要执行的function
+    service.setAfterFile = function (afterFn) {
+      service.afterFile = afterFn;
+    };
+
     service.send = function (url, postdata, cb) {
       if (!postdata) {
         postdata = {};
       }
       (service.before || angular.noop)(url, postdata);
+      $log.debug('发送数据参数：', url, postdata);
       $http({
         method: 'POST',
         url: service.dataServer + url,
@@ -56,6 +94,52 @@
         function (data, status) {
           $log.debug(data, status);
           var after = (service.after || angular.noop)(data);
+          $log.debug('after的返回值：', after);
+          //返回break表示中断流程
+          if (after == 'break') {
+            return;
+          }
+          (cb || angular.noop)(data.data);
+        },
+        function (data, status) {
+          $log.error('处理数据发生错误:', data, status);
+          (cb || angular.noop)(errorInfo);
+        }
+      );
+    };
+
+    service.sendFile = function (url, files, postdata, cb) {
+      if (!postdata) {
+        postdata = {};
+      }
+      (service.beforeFile || angular.noop)(url, files, postdata);
+      $log.debug('文件上传参数：url===>', url, ",files===>", files, ",postdata===>", postdata);
+      var form = new FormData();
+      //处理表单参数
+      postdata = service.jsonToFlat(postdata, '');
+      for (key in postdata) {
+        form.append(key, postdata[key]);
+      }
+      //处理文件信息(格式为上传文件的key（只能有一个）,值是文件数组{uploadkey:[file0,file1,...]})
+      for (key in files) {
+        var filelist = files[key];
+        for (var i = 0; i < filelist.length; i++) {
+          form.append(key, filelist[i]);
+        }
+      }
+      //上传文件
+      $http({
+        method: 'POST',
+        url: service.dataServer + url,
+        data: form,
+        headers: {
+          'Content-Type': undefined
+        },
+        transformRequest: angular.identity
+      }).then(
+        function (data, status) {
+          $log.debug(data, status);
+          var after = (service.afterFile || angular.noop)(data);
           $log.debug('after的返回值：', after);
           //返回break表示中断流程
           if (after == 'break') {
