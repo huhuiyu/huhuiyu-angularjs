d26eaee8a608c2cee9d7b8a83f380303296309ff
huhuiyu <3029679667@qq.com>
2019-02-13 13:11:30 +0800

    添加了简单的ajax调用测试工具

---
+++ angularjs.ajaxtool/app/js/controllers/IndexCtrl.js
@@ -0,0 +1,37 @@
+(function() {
+  var ctrls = angular.module(MyAppConfig.controllers);
+  ctrls.controller('IndexCtrl', ['$scope', '$log', 'MyDataService', 'DialogService', 'MyUtilService', IndexCtrl]);
+
+  function IndexCtrl($scope, $log, MyDataService, DialogService, MyUtilService) {
+    $log.debug('IndexCtrl init...');
+
+    // 处理scope销毁
+    $scope.$on('$destroy', function() {
+      $log.debug('IndexCtrl destroy...');
+    });
+
+    $scope.config = {
+      server: 'http://127.0.0.1:20000',
+      url: '/',
+      params: '{"echo":"哈哈"}'
+    };
+    $scope.result = '';
+    $scope.link = '';
+
+    $scope.test = function() {
+      DialogService.showWait('测试中...');
+      try {
+        MyDataService.setDataServer($scope.config.server);
+        var jsonData = JSON.parse($scope.config.params);
+        $scope.link = $scope.config.server + '?' + MyUtilService.jsonToDeeplink(MyDataService.jsonToFlat(jsonData));
+        MyDataService.send($scope.config.url, jsonData, function(data) {
+          DialogService.hideWait();
+          $scope.result = MyUtilService.trustAsHtml(MyUtilService.formatJson(data, true));
+        });
+      } catch (ex) {
+        DialogService.hideWait();
+        $scope.result = MyUtilService.trustAsHtml(MyUtilService.formatJson(ex, true));
+      }
+    };
+  }
+})();
